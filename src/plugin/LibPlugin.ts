376eadb2e82f03993a301110a2220943c12f902b
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-14 03:05:15 +0100

    Added more defaults to CommonPlugin
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/plugin/LibPlugin.ts
+++ src/plugin/LibPlugin.ts
@@ -3,11 +3,7 @@ import type {ConfigBuilder, ConfigBuilderPlugin, HookMap} from '../ConfigBuilder
 export class LibPlugin implements ConfigBuilderPlugin {
   apply(builder: ConfigBuilder, hooks: HookMap) {
     hooks.build.tapPromise(LibPlugin.name, async () => {
-      builder.set(`experiments.outputModule`, true)
       builder.set(`output.library.type`, `module`)
-      builder.set(`output.filename`, `[name].js`)
-      builder.set(`output.chunkFilename`, `[name].js`)
-      builder.set(`output.chunkFormat`, `module`)
     })
   }
 }
